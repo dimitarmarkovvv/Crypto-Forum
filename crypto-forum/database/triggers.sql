cc287cbbf79c2509d3f1151c9b1602b55a57da98
Dimitar Markov <mitko__pz132123232@abv.bg>
2026-09-19 04:00:35 +0300

    Implement user registration, closes #4

---
+++ crypto-forum/database/triggers.sql
@@ -0,0 +1,24 @@
+-- Auto-create profile row when a new auth user registers
+
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer
+set search_path = public
+as $$
+begin
+  insert into public.profiles (id, username, first_name, last_name, email)
+  values (
+    new.id,
+    new.raw_user_meta_data ->> 'username',
+    new.raw_user_meta_data ->> 'first_name',
+    new.raw_user_meta_data ->> 'last_name',
+    new.email
+  );
+  return new;
+end;
+$$;
+
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute function public.handle_new_user();
